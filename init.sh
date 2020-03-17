4f3319ca6cf2e35660d747cd238606e17ede13dc
slpcode <1370808424@qq.com>
2020-03-17 17:12:10 +0800

    update

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker network create app_net --subnet=172.16.238.0/24
-docker volume create --name=composer-data56
-docker volume create --name=composer-data71
-docker volume create --name=composer-data72
-docker volume create --name=composer-data73
-
-docker volume create --name=portainer_data
